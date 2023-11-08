128be5e254855ee68e8cfbe1fa44dd529ae44745
yduman <yusufdumans61@gmail.com>
2023-11-08 22:30:52 +0300

    Created ID Generators
    ID Generators for studentID, staffID, personID.

---
+++ CourseRegistrationSystem/src/Domain/PersonIDGenerator.java
@@ -0,0 +1,28 @@
+package Domain;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class PersonIDGenerator extends IDGenerator {
+    
+    public PersonIDGenerator(){
+        super();
+        this.setDigitCount(8);
+        this.setIdPrefix("999");
+        this.setIdSuffix("");
+        this.setUsedIDs(GetUsedIDsByOtherPeople());
+    }
+
+    private List<String> GetUsedIDsByOtherPeople(){
+
+        Collection<Person> allPersons = Department.getAllPeople();
+        List<String> alreadyUsedIDs = new ArrayList<>();
+
+        for (Person person : allPersons) {
+            alreadyUsedIDs.add(person.getPersonID());
+        }
+        
+        return alreadyUsedIDs; 
+    }
+}
