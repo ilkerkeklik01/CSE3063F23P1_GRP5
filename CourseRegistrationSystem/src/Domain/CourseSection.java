8515fa2e7ea1ce7aa7a182706b68e62dad465afc
Sarp Siraç Müftüler <61777991+sarpac@users.noreply.github.com>
2023-11-10 18:20:21 +0300

    Update CourseSection.java

--- CourseRegistrationSystem/src/Domain/CourseSection.java
+++ CourseRegistrationSystem/src/Domain/CourseSection.java
@@ -1,4 +1,47 @@
 package Domain;
+class CourseSection{
 
-public class CourseSection {
+    private String courseCode;
+    private int courseSchedule;
+    private int examCount;
+    private int credit;
+
+    public CourseSection(String courseCode, int courseSchedule, int credit, int examCount){
+        this.courseCode = courseCode;
+        this.credit = credit;
+        this.courseSchedule = courseSchedule;
+        this.examCount = examCount;
+    }
+
+    public String getcourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    public int getCourseSchedule() {
+        return courseSchedule;
+    }
+
+    public void setCourseSchedule(int courseSchedule) {
+        this.courseSchedule = courseSchedule;
+    }
+
+    public int getExamCount() {
+        return examCount;
+    }
+
+    public void setExamCount(int examCount) {
+        this.examCount = examCount;
+    }
 }
