70275ec4b56c49184fccf56396745ab9cc915e46
Havva Ozen <ozen.havvanurnur@gmail.com>
2023-11-14 18:32:26 +0300

    Show transcript info

--- CourseRegistrationSystem/src/Domain/Transcript.java
+++ CourseRegistrationSystem/src/Domain/Transcript.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 public class Transcript {
     private final List<Grade> grades;
+    private int completedCredits;
+
+    public Transcript() {
+        this.grades = new ArrayList<>();
+    }
 
     public int getCompletedCredits() {
         return completedCredits;
@@ -15,33 +20,47 @@ public class Transcript {
         this.completedCredits = completedCredits;
     }
 
-    private int completedCredits;
-    public Transcript() {
-        this.grades = new ArrayList<>();
-    }
+
 
 
     public void addGrade(Course course, int numericGrade) {
         Grade newGrade = new Grade(course, numericGrade);
         grades.add(newGrade);
+        updateTotalCredits();
     }
 
     public List<Grade> getGrades() {
         return grades;
     }
 
+    public void printTakenCoursesStatus() {
+        System.out.println("Completed Credits: " + getCompletedCredits() + "\n");
+        System.out.println("Taken Courses :");
+        printPassedCourses();
+        System.out.println();
+        printFailedCourses();
+    }
     public void printPassedCourses() {
         System.out.println("Passed Courses:");
         for (Grade grade : grades) {
             if (grade.isPassed()) {
-                System.out.println(grade.getCourse().getCourseName() + "is passed");
+                System.out.println(grade.getCourse().getCourseName() + " " + grade.getCourse().getCourseCode() + grade.getCourse().getCredit());
             }
         }
-
-
+    }
+    public void printFailedCourses() {
+        System.out.println("Failed Courses:");
+        for (Grade grade : grades) {
+            if (!grade.isPassed()) {
+                System.out.println(grade.getCourse().getCourseName() + " " + grade.getCourse().getCourseCode() + grade.getCourse().getCredit());
+            }
+        }
     }
 
-    public void calculateCompletedCredits(){
+
+
+
+    public void updateTotalCredits(){
         int totalCredits = 0;
         for(Grade grade: grades){
             totalCredits += grade.getCourse().getCredit();
