3d028f57f210d67c9381564baf9722d0f9202c85
Ilker Keklik <ilkerkeklik@marun.edu.tr>
2023-11-17 16:06:13 +0300

    Unit tests made by Sarp Siraç Müftüler are added.

---
+++ CourseRegistrationSystem/src/Tests/StudentTest.java
@@ -0,0 +1,56 @@
+package Tests;
+
+import Domain.Advisor;
+import Domain.Course;
+import Domain.Department;
+import Domain.Student;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudentTest {
+
+
+    @Test
+    public void testRegisterToNewCourse() {
+
+        ArrayList<String> courses = new ArrayList<>();
+        courses.add("CSE1000");
+
+        ArrayList<String> registrationNos = new ArrayList<>();
+        registrationNos.add("100");
+
+        ArrayList<String> lecturerNos = new ArrayList<>();
+        registrationNos.add("200");
+
+        ArrayList<String> studentNos = new ArrayList<>();
+        registrationNos.add("123");
+
+        Student student = new Student("abc","def",null,"123",courses,"2",registrationNos);
+
+        Course course = new Course("course","CSE1000",lecturerNos,null,studentNos,null,2);
+
+        course.setPrerequisitesIds(Arrays.asList());
+
+        Department.getInstance().getAllCourses().add(course);
+
+        Advisor advisor = new Advisor();
+        Department.getInstance().getAllAdvisors().add(advisor);
+
+        student.registerToNewCourse(course.getCourseCode(),"200");
+
+        assertTrue(student.getRegistrationNumbers().contains("200"));
+
+        assertTrue(advisor.getRegistrationNumbers().contains("200"));
+
+        assertTrue(Department.getInstance().getAllRegistrations().stream().anyMatch(registration -> registration.getRegistrationNo().equals("1")));
+    }
+
+}
